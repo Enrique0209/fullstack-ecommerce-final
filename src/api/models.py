781e57aa7ac7189d899c8378848371027a373128
Enrique González <enrique.glez.jai@gmail.com>
2026-06-26 17:12:46 +0000

    Avance 1

--- src/api/models.py
+++ src/api/models.py
@@ -9,11 +9,15 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    is_horeca: Mapped[bool] = mapped_column(Boolean(), nullable=False, default=False)
+    name: Mapped[str]= mapped_column(String(80), nullable=False)
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "is_horeca": self.is_horeca,
+            "name": self.name,
             # do not serialize the password, its a security breach
         }
