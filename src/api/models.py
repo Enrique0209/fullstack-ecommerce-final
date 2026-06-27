fa51ef5aa71620d7554d252c4c146d89b43d8f8b
Enrique González <enrique.glez.jai@gmail.com>
2026-06-27 17:32:34 +0000

    endopoint registro funcionando

--- src/api/models.py
+++ src/api/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False, default=True)
     is_horeca: Mapped[bool] = mapped_column(Boolean(), nullable=False, default=False)
     name: Mapped[str]= mapped_column(String(80), nullable=False)
 
