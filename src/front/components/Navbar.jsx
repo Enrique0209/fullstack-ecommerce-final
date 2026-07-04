f15f3beb2af54a3df5060baa2c979b9b42d80d03
Enrique González <enrique.glez.jai@gmail.com>
2026-07-04 18:28:59 +0000

    navbar con links de navegacion

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -6,8 +6,11 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">In Vino Veritas</span>
 				</Link>
+				<Link to="/catalog">Catálogo</Link>
+				<Link to="/login">Login</Link>
+				<Link to="/register">Registro</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
 						<button className="btn btn-primary">Check the Context in action</button>
