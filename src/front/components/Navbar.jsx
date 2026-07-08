03ab61138d0efdb59006bb9fb9d82ddf56917586
Enrique González <enrique.glez.jai@gmail.com>
2026-07-08 12:15:20 +0000

    Link navbar solo para admin

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const Navbar = () => {
+    const { store } = useGlobalReducer()
+    
     return (
         <nav className="navbar navbar-dark bg-dark px-4">
             <Link to="/" className="navbar-brand fw-bold">🍹 In Vino Veritas</Link>
@@ -10,6 +13,9 @@ export const Navbar = () => {
                 <Link to="/profile" className="nav-link text-white">Perfil</Link>
                 <Link to="/login" className="btn btn-outline-light btn-sm">Login</Link>
                 <Link to="/register" className="btn btn-warning btn-sm">Registro</Link>
+                {store.user?.is_admin && (
+                    <Link to="/admin" className="btn btn-danger btn-sm">Admin</Link>
+                )}
             </div>
         </nav>
     );
